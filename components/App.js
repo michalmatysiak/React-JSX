2530d726c22158f107e81f31c60be1438ace0be8
michalmatysiak <37140076+michalmatysiak@users.noreply.github.com>
2018-04-25 13:02:22 +0200

    Update App.js

--- components/App.js
+++ components/App.js
@@ -29,7 +29,7 @@ var App = React.createClass({
   render: function() {
     return (
       <div className={'app'}>
-        <ContactForm={contact:contactForm}/>
+        <ContactForm contact={contactForm}/>
         <Contacts items={contacts}/>
       </div>
     );
